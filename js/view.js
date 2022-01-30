577650fc05dd4d7d6ba07850d5ab5ec477690825
jonathan <jonathan.santosmartins@gmail.com>
2022-01-30 17:26:44 -0300

    feat: load spinner

--- js/view.js
+++ js/view.js
@@ -1,10 +1,17 @@
 const inputEl = document.querySelector("input");
 const tableElement = document.querySelector(".table-elements");
+const spinnerEl = document.querySelector(".spinner-wrapper");
 
 export const insertElementsIntoContainer = (tabuada) => {
+  showSpinner();
   tableElement.innerHTML = tabuada
     .map(({ num, resultado }) => {
       return `<li class="table-elements-li">${inputEl.value} x ${num} = ${resultado}</li>`;
     })
     .join("");
 };
+
+export function showSpinner() {
+  spinnerEl.style.display = "block";
+  setTimeout(() => (spinnerEl.style.display = "none"), 2000);
+}
