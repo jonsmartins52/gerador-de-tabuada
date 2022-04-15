8ab7fb6cc06a954e3104c71871c00d62d4e70157
jonathan <jonathan.santosmartins@gmail.com>
2022-04-15 14:53:39 -0300

    feat: Objeto DARK_MODE

--- misc/utils.js
+++ misc/utils.js
@@ -1,3 +1,13 @@
 export const utils = {
   LOADER_TIME: 2000,
+
+  DARK_MODE: {
+    body: "dark-mode",
+    header: "dark-mode__header",
+    icon: "dark-mode__icon",
+    input: "dark-mode__input",
+    submit: "dark-mode__submit",
+    clean: "dark-mode__clean",
+    table: "dark-mode__table",
+  },
 };
